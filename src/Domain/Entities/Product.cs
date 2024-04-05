211964e3d42c1ddfbc4f01f20e285f82f34261c3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-05 18:50:20 +0500

    Entities

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string VideoPath { get; set; }
+        public int SortNumber { get; set; }
+    }
+}
