44d127a6dd699a41b0f0e1535e5113f8919adb15
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-05 18:49:16 +0500

    IGenericRepository Created

---
+++ src/Application/Abstractions/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Abstractions
+{
+    public interface IGenericRepository<TEntity>
+    {
+        public ValueTask<TEntity> CreateAsync(TEntity entity);
+        public IQueryable<TEntity> GetAllAsync();
+        public ValueTask<TEntity> GetByIdAsync(int id);
+        public ValueTask<TEntity> UpdateAsync(TEntity entity);
+        public ValueTask<TEntity> DeleteAsync(int Id);
+    }
+}
