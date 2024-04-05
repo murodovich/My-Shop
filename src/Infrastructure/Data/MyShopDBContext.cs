8d434ad38d34bf292f56786bcc2e6af4cebe2e77
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-05 18:50:37 +0500

    Migration

---
+++ src/Infrastructure/Data/MyShopDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class MyShopDBContext : DbContext
+    {
+        public MyShopDBContext(DbContextOptions<MyShopDBContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Product> products {  get; set; }
+    }
+}
