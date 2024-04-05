2a7f6586a348d0dd53b4433a704eebaf61a17e44
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-05 18:50:14 +0500

    Application

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Application.FileServices;
+using Application.Services.Contracts.Products;
+using Application.Services.Products;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddScoped<IFileService, FileService>();
+
+            return services;
+        }
+    }
+}
