a890709520ab203e42693fefa63eb84e88a1fbaf
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-05 18:51:06 +0500

    Presentation

---
+++ src/Presentation/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Dtos.Produts;
+using Application.Services.Contracts.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateProductAsync([FromForm]CreateProductDto createProductDto)
+        {
+            var result = await _productService.AddAsync(createProductDto);
+
+            return Ok(result);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var products = await _productService.GetAllAsync();
+
+            return Ok(products);
+
+        }
+
+    }
+}
