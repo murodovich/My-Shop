7a8a77db27cdf9c534a8462a28fc2bd27ab71cbf
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-05 18:50:57 +0500

    Infrastructure

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Products;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+
+
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<MyShopDBContext>(options =>
+                options
+                .UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            
+            
+            return services;
+        }
+
+    }
+}
