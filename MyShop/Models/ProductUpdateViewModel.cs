5dfb1ee3c9cec25821f064ca4e5a597fac5746b1
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-07 19:23:51 +0500

    Models Created

---
+++ MyShop/Models/ProductUpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyShop.Models
+{
+    public class ProductUpdateViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile? Videopath { get; set; }
+        public int SortNumber { get; set; }
+    }
+}
