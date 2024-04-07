732b9223a0ab7a4b1cbbe35af6ca8cd455b5e430
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-04-07 19:25:31 +0500

    Myshop

---
+++ MyShop/Program.cs
@@ -0,0 +1,34 @@
+using Application;
+using Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication(builder.Configuration);
+
+var app = builder.Build();
+
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Product}/{action=Index}/{id?}");
+
+app.Run();
